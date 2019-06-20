fc6224a8d091bddbac7a897a5c6a92dc38741621
ranjithlucifer <51074137+ranjithlucifer@users.noreply.github.com>
2019-06-20 12:21:34 +0530

    Update smallsubstring.py

--- smallsubstring.py
+++ smallsubstring.py
@@ -1,25 +1,28 @@
-a=256
-def maximum(b,n):
-  count=[0]*a
-  for i in range(n): 
-      count[ord(str[i])]+=1
-   x=0
-   for i in range(a): 
-      if (count[i]!=0): 
-          x+=1    
-   return x 
-def smallsubstring(b): 
-    n=len(b)    
-    x=maximum(b, n) 
-    y=n
+a= 256
+def max_distinct_char(str, n): 
+    count = [0] * a
+    for i in range(n): 
+        count[ord(str[i])] += 1
+    max_distinct = 0
+    for i in range(a): 
+        if (count[i] != 0): 
+            max_distinct += 1    
+    return max_distinct 
+def smallesteSubstr_maxDistictChar(str): 
+    n = len(str)     
+    max_distinct = max_distinct_char(str, n) 
+    minl = n     
     for i in range(n): 
         for j in range(n): 
-            s=str[i:j] 
-            s1=len(subs) 
-            s2=maximum(s,s1)  
-            if (s1<y and x==s2): 
-                y=s1 
-    return y 
-l=smallsubstring(b)
-b=str(input())
-print(l)
+            subs = str[i:j] 
+            subs_lenght = len(subs) 
+            sub_distinct_char = max_distinct_char(subs,  
+                                                  subs_lenght) 
+            if (subs_lenght < minl and 
+                max_distinct == sub_distinct_char): 
+                minl = subs_lenght 
+    return minl 
+if __name__ == "__main__": 
+    str = str(input())
+    l = smallesteSubstr_maxDistictChar(str); 
+    print(l) 
